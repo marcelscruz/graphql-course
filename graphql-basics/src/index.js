5035c484d9e367769d05627964efe4570dcbd742
Marcel Cruz <marcelcruz@live.com>
2018-12-22 22:27:59 +0100

    Add type definitions with scalar types and resolvers

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,44 @@
-console.log('hello graphql')
+import { GraphQLServer } from 'graphql-yoga'
+
+// Scalar types - String, Boolean, Int, Float, ID
+
+// Type definitions (schema)
+const typeDefs = `
+  type Query {
+    title: String!
+    price: Float!
+    releaseYear: Int
+    rating: Float
+    inStock: Boolean!
+  }
+`
+
+// Resolvers
+const resolvers = {
+  Query: {
+    title() {
+      return 'Product title here'
+    },
+    price() {
+      return '2.20'
+    },
+    releaseYear() {
+      return null
+    },
+    rating() {
+      return 2.2
+    },
+    inStock() {
+      return true
+    },
+  },
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+})
+
+server.start(() => {
+  console.log('The server is up!')
+})
