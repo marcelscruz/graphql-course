8df87386e38c3f935c6014786a743399536cdca6
Marcel Cruz <marcelcruz@live.com>
2019-01-12 18:37:26 +0100

    add array

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -2,11 +2,56 @@ import { GraphQLServer } from 'graphql-yoga'
 
 // Scalar types - String, Boolean, Int, Float, ID
 
+// Demo user data
+const users = [
+  {
+    id: '1',
+    name: 'Marcel',
+    email: 'marcelcruz@live.com',
+    age: 27,
+  },
+  {
+    id: '2',
+    name: 'Sarah',
+    email: 'sarah@email.com',
+  },
+  {
+    id: '3',
+    name: 'Mike',
+    email: 'mike@email.com',
+  },
+]
+
+const posts = [
+  {
+    id: '1',
+    title: 'Post number one',
+    body: 'This is post number one.',
+    published: true,
+  },
+  {
+    id: '2',
+    title: 'Post number two',
+    body: 'This is post number two.',
+    published: false,
+  },
+  {
+    id: '1',
+    title: 'Post number three',
+    body: 'This is post number three.',
+    published: true,
+  },
+]
+
+// greeting(name: String, position: String): String!
+// add(numbers: [Float!]!): Float!
+// grades: [Int!]!
+
 // Type definitions (schema)
 const typeDefs = `
   type Query {
-    greeting(name: String, position: String): String!
-    add(a: Float!, b: Float!): Float!
+    users(query: String): [User!]!
+    posts(query: String): [Post!]!
     me: User!
     post: Post!
   }
@@ -29,14 +74,30 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
   Query: {
-    greeting(parent, args, ctx, info) {
-      if (args.name && args.position) {
-        return `Hello, ${args.name}! You're my favourite ${args.position}.`
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users
       }
-      return 'Hello'
+
+      return users.filter(user =>
+        user.name.toLowerCase().includes(args.query.toLowerCase()),
+      )
     },
-    add(parent, args, ctx, info) {
-      return args.a + args.b
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
+        return posts
+      }
+
+      return posts.filter(post => {
+        const isTitleMatch = post.title
+          .toLowerCase()
+          .includes(args.query.toLowerCase())
+        const isBodyMatch = post.body
+          .toLowerCase()
+          .includes(args.query.toLowerCase())
+
+        return isTitleMatch || isBodyMatch
+      })
     },
     me() {
       return {
@@ -56,6 +117,23 @@ const resolvers = {
     },
   },
 }
+
+// greeting(parent, args, ctx, info) {
+//   if (args.name && args.position) {
+//     return `Hello, ${args.name}! You're my favourite ${args.position}.`
+//   }
+//   return 'Hello'
+// },
+// add(parent, args, ctx, info) {
+//   if (args.numbers.length === 0) {
+//     return 0
+//   }
+
+//   return args.numbers.reduce((acc, number) => acc + number)
+// },
+// grades(parent, args, ctx, info) {
+//   return [99, 80, 93]
+// },
 
 const server = new GraphQLServer({
   typeDefs,
